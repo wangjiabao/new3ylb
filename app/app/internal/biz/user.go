743d91c4cc519ed49f48f66f707278b77d7d8285
wangjiabao <840028401@qq.com>
2023-04-05 21:48:08 +0800

    ok

--- app/app/internal/biz/user.go
+++ app/app/internal/biz/user.go
@@ -1684,8 +1684,8 @@ func (uuc *UserUseCase) AddUserBnbAmount(ctx context.Context, userRewardMap map[
 
 	for userId, vUserRewardMap := range userRewardMap {
 
-		fmt.Println(userId, vUserRewardMap, vUserRewardMap*rate, usersMap[userId])
-		fmt.Println(userId, vUserRewardMap, vUserRewardMap*rate, usersMap[userId])
+		fmt.Println(vUserRewardMap, vUserRewardMap*rate, usersMap[userId])
+		fmt.Println(vUserRewardMap, vUserRewardMap*rate, usersMap[userId])
 
 		// 新增
 		//err = uuc.ubRepo.AddBnbAmount(ctx, userId, vUserRewardMap*rate)
