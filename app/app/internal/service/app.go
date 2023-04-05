e2fcf8a9515c459d4a50c7c8b242643adaae1e2d
wangjiabao <840028401@qq.com>
2023-04-05 20:51:51 +0800

    ok

--- app/app/internal/service/app.go
+++ app/app/internal/service/app.go
@@ -614,23 +614,23 @@ func (a *AppService) RewardAllUserBnbBalance(ctx context.Context, req *v1.Reward
 		if 1 == k {
 			fmt.Println(len(vUserReward))
 			tmpBuyAmount = buyRewardAmount * 100 / 10 / float64(len(vUserReward))
-			tmpSellAmount = buyRewardAmount * 100 / 20 / float64(len(vUserReward))
+			tmpSellAmount = sellRewardAmount * 100 / 20 / float64(len(vUserReward))
 		} else if 2 == k {
 			fmt.Println(len(vUserReward))
 			tmpBuyAmount = buyRewardAmount * 100 / 15 / float64(len(vUserReward))
-			tmpSellAmount = buyRewardAmount * 100 / 20 / float64(len(vUserReward))
+			tmpSellAmount = sellRewardAmount * 100 / 20 / float64(len(vUserReward))
 		} else if 3 == k {
 			fmt.Println(len(vUserReward))
 			tmpBuyAmount = buyRewardAmount * 100 / 20 / float64(len(vUserReward))
-			tmpSellAmount = buyRewardAmount * 100 / 20 / float64(len(vUserReward))
+			tmpSellAmount = sellRewardAmount * 100 / 20 / float64(len(vUserReward))
 		} else if 4 == k {
 			fmt.Println(len(vUserReward))
 			tmpBuyAmount = buyRewardAmount * 100 / 25 / float64(len(vUserReward))
-			tmpSellAmount = buyRewardAmount * 100 / 20 / float64(len(vUserReward))
+			tmpSellAmount = sellRewardAmount * 100 / 20 / float64(len(vUserReward))
 		} else if 5 == k {
 			fmt.Println(len(vUserReward))
 			tmpBuyAmount = buyRewardAmount * 100 / 30 / float64(len(vUserReward))
-			tmpSellAmount = buyRewardAmount * 100 / 20 / float64(len(vUserReward))
+			tmpSellAmount = sellRewardAmount * 100 / 20 / float64(len(vUserReward))
 		}
 
 		for _, vVUserReward := range vUserReward {
