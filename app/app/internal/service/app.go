82f7c7e0e5bb609a0c850e03d7e33757cd66b8a9
wangjiabao <840028401@qq.com>
2023-04-07 02:56:11 +0800

    ok

--- app/app/internal/service/app.go
+++ app/app/internal/service/app.go
@@ -687,29 +687,10 @@ func (a *AppService) RewardAllUserBnbBalance(ctx context.Context, req *v1.Reward
 		}
 	}
 
-	//var (
-	//	price string
-	//	rate  float64
-	//)
-	//price, err = GetAmountOut("10000000000")
-	//if nil != err {
-	//	return nil, err
-	//}
-	//
-	//rate, err = strconv.ParseFloat(price, 64)
-	//if nil != err {
-	//	return nil, err
-	//}
-
-	//rateFloat, _ := strconv.ParseFloat(fmt.Sprintf("%.7f", rate/10000000000), 64)
-	//if rateFloat <= 0 {
-	//	return nil, errors.New(500, "ERROR_TOKEN", "分红比率错误")
-	//}
-
-	//err = a.uuc.AddUserBnbAmount(ctx, userRewardMap, usersMap)
-	//if nil != err {
-	//	return nil, err
-	//}
+	err = a.uuc.AddUserBnbAmount(ctx, userRewardMap, usersMap)
+	if nil != err {
+		return nil, err
+	}
 
 	return &v1.RewardAllUserBnbBalanceReply{}, nil
 }
