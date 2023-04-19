37327517eb38b100b8e60355e320f705764a190f
wangjiabao <840028401@qq.com>
2023-04-20 00:16:43 +0800

    ok

--- app/app/internal/biz/user.go
+++ app/app/internal/biz/user.go
@@ -863,7 +863,7 @@ func (uuc *UserUseCase) UserInfo(ctx context.Context, user *User) (*v1.UserInfoR
 		RecommendTotal:              fmt.Sprintf("%.2f", float64(recommendTotal)/float64(10000000000)),
 		LocationTotal:               fmt.Sprintf("%.2f", float64(locationTotal)/float64(10000000000)),
 		Usdt:                        "0x55d398326f99059fF775485246999027B3197955",
-		Account:                     "0x6efcfdea401f7e5c418e56e9468ff32238c3da36",
+		Account:                     "0x5e30db5983170028d09ed5d7cfb25aa6495334c8",
 		AmountB:                     fmt.Sprintf("%.2f", float64(myLastLocationCurrent)/float64(10000000000)),
 		UserCount:                   userCount,
 		TotalDeposit:                fmt.Sprintf("%.2f", float64(totalDepoist)/float64(10000000000)),
